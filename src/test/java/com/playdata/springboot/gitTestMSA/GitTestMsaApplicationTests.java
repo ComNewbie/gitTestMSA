37bf9e2a10e813b4cd85ce58fd7cf78b444ba97f
ComNewbie <iforking3@naver.com>
2023-10-23 10:21:08 +0900

    github 프로젝트 올려놨습니다. fork해가세요

---
+++ src/test/java/com/playdata/springboot/gitTestMSA/GitTestMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.springboot.gitTestMSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestMsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
